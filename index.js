80d7e13bd6b08ef330c5347d2c942865dbd86178
Vojta Jina <vojta.jina@gmail.com>
2013-01-26 20:00:26 -0800

    Initial commit, extracted from https://github.com/testacular/testacular

---
+++ index.js
@@ -0,0 +1,27 @@
+var coffee = require('coffee-script');
+
+var createCoffeePreprocessor = function(logger, basePath) {
+  var log = logger.create('preprocessor.coffee');
+
+  return function(content, file, done) {
+    var processed = null;
+
+    log.debug('Processing "%s".', file.originalPath);
+    file.path = file.originalPath + '-compiled.js';
+
+    try {
+      processed = coffee.compile(content, {bare: true});
+    } catch (e) {
+      log.error('%s\n  at %s', e.message, file.originalPath);
+    }
+
+    done(processed);
+  };
+};
+
+createCoffeePreprocessor.$inject = ['logger', 'config.basePath'];
+
+// PUBLISH DI MODULE
+module.exports = {
+  'preprocessor:coffee': ['factory', createCoffeePreprocessor]
+};
