486abccb97ad4e5d13a44df8a687d9011ae6d8e3
Vojta Jina <vojta.jina@gmail.com>
2013-07-05 15:42:17 -0700

    feat: make options, transformPath configurable
    Closes #1

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 var coffee = require('coffee-script');
 
-var createCoffeePreprocessor = function(logger, basePath) {
+var createCoffeePreprocessor = function(args, config, logger, helper) {
+  config = config || {};
+
   var log = logger.create('preprocessor.coffee');
+  var defaultOptions = {
+    bare: true
+  };
+  var options = helper.merge(defaultOptions, args.options || {}, config.options || {});
+
+  var transformPath = args.transformPath || config.transformPath || function(filepath) {
+    return filepath.replace(/\.coffee$/, '.js');
+  };
 
   return function(content, file, done) {
     var processed = null;
 
     log.debug('Processing "%s".', file.originalPath);
-    file.path = file.originalPath.replace(/\.coffee$/, '.js');
+    file.path = transformPath(file.originalPath);
 
     try {
-      processed = coffee.compile(content, {bare: true});
+      processed = coffee.compile(content, options);
     } catch (e) {
       log.error('%s\n  at %s', e.message, file.originalPath);
     }
@@ -19,7 +29,7 @@ var createCoffeePreprocessor = function(logger, basePath) {
   };
 };
 
-createCoffeePreprocessor.$inject = ['logger', 'config.basePath'];
+createCoffeePreprocessor.$inject = ['args', 'config.coffeePreprocessor', 'logger', 'helper'];
 
 // PUBLISH DI MODULE
 module.exports = {
