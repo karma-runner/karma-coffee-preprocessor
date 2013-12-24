f1d5e7ef090d21b7bc4608d1468fd7fbce21fbe7
Vojta Jina <vojta.jina@gmail.com>
2013-12-24 01:56:10 -0800

    feat: support source maps (rewriting stack traces)
    https://github.com/karma-runner/karma/commit/70e4abd9b8db6b05de557ca6e9204339a21be06b

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ var createCoffeePreprocessor = function(args, config, logger, helper) {
       map.sources[0] = path.basename(file.originalPath)
       map.sourcesContent = [content]
       map.file = path.basename(file.path)
+      file.sourceMap = map;
       datauri = 'data:application/json;charset=utf-8;base64,' + new Buffer(JSON.stringify(map)).toString('base64')
       done(result.js + '\n//@ sourceMappingURL=' + datauri + '\n');
     } else {
